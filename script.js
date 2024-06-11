2420eadf137fca987664368dbacd315c7f39ad74
Paulus sesa <114337187+seanpaulsesa@users.noreply.github.com>
2024-06-11 03:59:55 -0700

    fix: menambahkan file index.html, file style.css, file script.js

---
+++ script.js
@@ -0,0 +1,17 @@
+// document.getElementById("pesan").textContent = "Helo sacode!";
+// .querySelector('#pesan')
+// .textContent = "selamat datang"
+
+// document
+// .querySelector(".selamat");
+//     .textContent  = "Happy Coding";
+
+var elemenBaru = document
+.createElement("strong");
+elemenBaru.textContent = "di SaCode Community!";
+
+Document.getElementbyid("pesan").appendchild(elemenBaru);
+
+document
+.querySelector(".selamat")
+.textContent = "Happy Coding!";
